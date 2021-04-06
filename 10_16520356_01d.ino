70ebb4b265ff3fb5854703a4c305f8dbc8eca078
alifiyabs <alifiyabshary@gmail.com>
2021-04-06 21:21:38 +0700

    init repo 01d

--- 10_16520356_01d.ino
+++ 10_16520356_01d.ino
@@ -14,7 +14,7 @@ void loop()
   for (counter = 0; counter < 3; ++counter) {
     digitalWrite(7, HIGH);
     delay(1000); // Wait for 1000 millisecond(s)
-    digitalWrite(7, HIGH);
+    digitalWrite(7, LOW);
     delay(1000); // Wait for 1000 millisecond(s)
   }
   delay(5000); // Wait for 5000 millisecond(s)
