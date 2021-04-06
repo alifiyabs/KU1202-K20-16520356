17b213c797c30aef80d65317483792e460e852aa
alifiyabs <alifiyabshary@gmail.com>
2021-04-06 21:49:45 +0700

    revisi 1

--- 10_16520356_01d.ino
+++ 10_16520356_01d.ino
@@ -14,7 +14,11 @@ void loop()
   for (counter = 0; counter < 3; ++counter) {
     digitalWrite(7, HIGH);
     delay(1000); // Wait for 1000 millisecond(s)
+<<<<<<< HEAD
     digitalWrite(7, LOW);
+=======
+    digitalWrite(7, HIGH);
+>>>>>>> parent of 848a9ed (Delete 10_16520356_01d.ino)
     delay(1000); // Wait for 1000 millisecond(s)
   }
   delay(5000); // Wait for 5000 millisecond(s)
